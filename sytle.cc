9f881ff155ba5de2348b18dd76cf79af6afcd3c6
shellinford <47420544+shellinford@users.noreply.github.com>
2021-01-28 22:48:05 +0900

    Create sytle.cc

---
+++ sytle.cc
@@ -0,0 +1,71 @@
+.one{
+  background-image: url("img/back-img.png");
+  background-size: cover;
+  height:80vh;
+  padding-top:50px;
+}
+
+h1{
+  color:white;
+  font-family: serif;
+}
+
+
+header{
+  display: flex;
+  justify-content: space-between;
+}
+
+.select ul{
+  display: flex;
+  font-size: 17px;
+  margin-right: 60px;
+  list-style: none;
+  font-family: serif;
+}
+
+.select li{
+  margin-right: 60px;
+}
+
+.select a{
+  text-decoration: none;
+  color: rgb(255,255,255,.8);
+}
+
+.select a:hover{
+  color: rgb(255, 255, 255,.5);
+}
+
+.logo a{
+  margin-left: 120px;
+  color: rgb(255,255,255);
+  text-decoration: none;
+  font-family: serif;
+}
+
+.logo a:hover{
+  color: rgb(255, 255, 255,.5);
+}
+
+main-header h1{
+  text-align: center;
+  margin-top: 15vh;
+  font-size: 40px;
+}
+
+.two{
+  height:100vh;
+}
+
+.two span{
+  border: 1px solid;
+  padding:3px 5px;
+}
+
+
+.two p{
+  padding-bottom: 40px;
+  border-bottom: 2px solid;
+  margin-top: 20px;
+}
